49ad0bb3eb7db74c97501f3cc19ad6caa5c4ed7f
vettelwang <vettelwang@vettelwangdeiMac.local>
2018-06-04 14:59:08 +0800

    update

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -4,33 +4,31 @@ const path = require("path");
 const commonConfig = require("./webpack.common.config.js");
 
 const devConfig = {
-  devtool: "inline-source-map",
-  entry: {
-    app: ["react-hot-loader/patch", path.join(__dirname, "src/index.js")]
-  },
-  output: {
-    filename: "[name].[hash].js"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader",'postcss-loader']
-      }
-    ]
-  },
-  devServer: {
-    contentBase: path.join(__dirname, "./dist"),
-    historyApiFallback: true
-  }
+    devtool: "inline-source-map",
+    entry: {
+        app: ["react-hot-loader/patch", path.join(__dirname, "src/index.js")]
+    },
+    output: {
+        filename: "[name].[hash].js"
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: ["style-loader", "css-loader", 'postcss-loader']
+        }]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "./dist"),
+        historyApiFallback: true
+    }
 };
 
 module.exports = merge({
-  customizeArray(a, b, key) {
-    /*entry.app不合并，全替换*/
-    if (key === "entry.app") {
-      return b;
+    customizeArray(a, b, key) {
+        /*entry.app不合并，全替换*/
+        if (key === "entry.app") {
+            return b;
+        }
+        return undefined;
     }
-    return undefined;
-  }
 })(commonConfig, devConfig);
